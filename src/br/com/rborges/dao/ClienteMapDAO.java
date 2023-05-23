b2e597b06b31678a7a57981754893abf0b5c469d
Rebeca Borges <87546461+bexxborgs@users.noreply.github.com>
2023-05-23 20:26:31 -0300

    Merge remote-tracking branch 'Cadastro/master'

--- src/br/com/rborges/dao/ClienteMapDAO.java
+++ src/br/com/rborges/dao/ClienteMapDAO.java
@@ -3,8 +3,8 @@ package br.com.rborges.dao;
 import br.com.rborges.domain.Cliente;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author Rebeca
@@ -12,28 +12,25 @@ import java.util.Map;
 public class ClienteMapDAO implements  IClienteDAO {
     private Map<Long, Cliente> map;
     public ClienteMapDAO() {
-        this.map = new HashMap<>();
+        map =  new TreeMap<>();}
 
-    }
 
     @Override
     public Boolean cadastrar(Cliente cliente) {
         if (this.map.containsKey(cliente.getCpf())) {
             return  false;
         }
-        this.map.put(cliente.getCpf(), cliente);
+
+        map.put(cliente.getCpf(), cliente);
         return true;
     }
 
     @Override
     public void excluir(Long cpf) {
-        Cliente clienteCasdastrado = this.map.get(cpf);
-
-        if (clienteCasdastrado != null) {
-            this.map.remove(clienteCasdastrado.getCpf(), clienteCasdastrado);
-        }
-
+        Cliente clienteCasdastrado = map.get(cpf);
+        map.remove(clienteCasdastrado.getCpf(), clienteCasdastrado);
     }
+
     @Override
     public void alterar(Cliente cliente) {
         Cliente clienteCadastrado = this.map.get(cliente.getCpf());
